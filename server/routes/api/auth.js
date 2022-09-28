e88809fcb9392a9332df6023abe6cb87903d7395
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-09-29 01:22:31 +0300

    login

---
+++ server/routes/api/auth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const { validation, ctrlWrapper } = require("../../middlewares");
+const { auth: ctrl } = require("../../controllers");
+const { joiRegisterSchema, joiLoginSchema } = require("../../models/user");
+
+const router = express.Router();
+
+router.post(
+  "/register",
+  validation(joiRegisterSchema),
+  ctrlWrapper(ctrl.register)
+);
+router.post("/login", validation(joiLoginSchema), ctrlWrapper(ctrl.login));
+
+module.exports = router;
