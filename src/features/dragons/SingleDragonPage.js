294b3cad0014246e660dba8e450940b7a37e9987
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-10-04 00:03:38 +0300

    new style

--- src/features/dragons/SingleDragonPage.js
+++ src/features/dragons/SingleDragonPage.js
@@ -2,6 +2,7 @@ import { Spinner } from "../../components/spinner/Spinner";
 import { useGetDragonQuery } from "../api/apiSlice";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import { Wrapper } from "./SingleDragonPage.styled";
 
 export const SingleDragonPage = ({ match }) => {
   const { dragonId } = match.params;
@@ -13,7 +14,7 @@ export const SingleDragonPage = ({ match }) => {
     content = <Spinner text="Loading..." />;
   } else if (isSuccess) {
     content = (
-      <article className="dragon-excerpt" style={{ display: "flex" }}>
+      <Wrapper>
         <Carousel showThumbs={false}>
           {dragon.flickr_images.map((image) => {
             return (
@@ -24,7 +25,7 @@ export const SingleDragonPage = ({ match }) => {
           })}
         </Carousel>
 
-        <div style={{ marginLeft: "20px" }}>
+        <div>
           <p>{dragon.name}</p>
           <p>{dragon.description}</p>
           <a href={dragon.wikipedia}>{dragon.wikipedia}</a>
@@ -32,14 +33,9 @@ export const SingleDragonPage = ({ match }) => {
           <p>{dragon.dry_mass_kg}</p>
           <p>{dragon.first_flight}</p>
         </div>
-      </article>
+      </Wrapper>
     );
   }
 
-  return (
-    <section className="singl-dragon">
-      <h2>Dragon</h2>
-      {content}
-    </section>
-  );
+  return <section className="singl-dragon">{content}</section>;
 };
---
+++ src/features/dragons/SingleDragonPage.styled.js
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+import { mediaTablet } from "../../functions";
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  gap: 20px;
+  padding: 15px;
+  color: white;
+  background-color: black;
+
+  ${mediaTablet(`
+    grid-template-rows: none;
+    grid-template-columns: repeat(2, 1fr);
+    background-color: black;
+    
+    padding: 20px;
+  `)};
+`;
